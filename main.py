c96404463771a436288944f8ca4fe77ee3243d21
Malik <74842691+ItsMalikx@users.noreply.github.com>
2024-09-18 23:21:33 +0000

    Basic chatbot interaction with menu options
    Created a simple chatbot that interacts with users by asking for their name and age.
    Implemented a display_menu() function to present three placeholder options and an exit option.
    Added user_selection() function to handle user input and respond based on the selected option.
    The chatbot ends the conversation gracefully if the user selects the exit option.

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print('Hello world!')
+print('Welcome to Elite 101 Chatbot!')
+
+name = input('Please enter your name: ')
+
+print(f'Nice to meet you {name}!')
+
+age = input('How old are you?: ')
+
+print(f'Welcome {name}! Oh to be {age} again! How can I help you today?\n')
+
+def display_menu():
+  print("Please Choose from the following options:")  
+  print("1. Placeholder Option 1") 
+  print("2. Placeholder Option 2")   
+  print("3. Placeholder Option 3")  
+  print("4. Exit the conversation\n")  
+
+display_menu()
+
+def user_selection():
+  user_choice = int(input('Enter a number between 1-4: '))
+  if user_choice == 1:
+    print('Placeholder Option 1')
+  elif user_choice == 2:
+    print('Placeholder Option 2')
+  elif user_choice == 3:
+    print('Placeholder Option 3')
+  elif user_choice == 4:
+    print(f'Goodbye {name}! Have a great day!') 
+    exit()
+
+user_selection()
